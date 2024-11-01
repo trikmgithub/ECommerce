5d71ad38b1b540d979ff01d8e1e15863eee29cae
trikmgithub <trikmforwork@gmail.com>
2024-11-01 19:31:48 +0700

    Video 3

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 var userSchema = new mongoose.Schema({
     firstname: {
@@ -52,5 +53,13 @@ var userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 module.exports = mongoose.model('User', userSchema);
