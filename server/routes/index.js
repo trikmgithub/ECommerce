18dc276ad4b2bf0734e8121c613af3caa5785c41
trikmgithub <trikmforwork@gmail.com>
2024-11-01 19:31:48 +0700

    Video 3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
-const userRouter = require('./user')
-// const { notFound, errHandler } = require('../middlewares/errHandler')
+const userRouter = require('./user');
+const { notFound, errHandler } = require('../middlewares/errHandler');
 const initRoutes = (app) => {
-    app.use('/api/user', userRouter)
-    // app.use(notFound)
-    // app.use(errHandler)
+    app.use('/api/user', userRouter);
+    app.use(notFound);
+    app.use(errHandler);
 }
-module.exports = initRoutes
+module.exports = initRoutes;
