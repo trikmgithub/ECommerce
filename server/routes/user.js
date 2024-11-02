106c39aeef0806c869d17df0bd69cdac93a0d884
trikmgithub <trikmforwork@gmail.com>
2024-11-02 16:18:58 +0700

    Video 4

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
 router.post('/register', ctrls.register)
+router.post('/login', ctrls.login)
+// router.get('/current', verifyAccessToken, ctrls.getCurrent)
 module.exports = router
 // CRUD | Create - Read - Update - Delete | POST - GET - PUT - DELETE
