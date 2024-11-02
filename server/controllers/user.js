adc5dc572ffdea8e707a7e33eaed7dd68f0dacbc
trikmgithub <trikmforwork@gmail.com>
2024-11-02 17:17:55 +0700

    Video 5

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,7 @@
-const User = require('../models/user')
-const asyncHandler = require('express-async-handler')
+const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const { generateAccessToken, generateRefreshToken } = require('../middlewares/jwt');
+
 const register = asyncHandler(async (req, res) => {
     const { email, password, firstname, lastname } = req.body
     if (!email || !password || !lastname || !firstname)
@@ -30,26 +32,36 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if (response && await response.isCorrectPassword(password)) {
         // Tách password và role ra khỏi response
-        const { password, role, ...userData } = response.toObject()
-        // // Tạo access token
-        // const accessToken = generateAccessToken(response._id, role)
-        // // Tạo refresh token
-        // const refreshToken = generateRefreshToken(response._id)
-        // // Lưu refresh token vào database
-        // await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
-        // // Lưu refresh token vào cookie
-        // res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        const { password, role, ...userData } = response.toObject();
+        // Tạo access token
+        const accessToken = generateAccessToken(response._id, role);
+        // Tạo refresh token
+        const refreshToken = generateRefreshToken(response._id)
+        // Lưu refresh token vào database
+        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
+        // Lưu refresh token vào cookie
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
         return res.status(200).json({
             sucess: true,
-            // accessToken,
+            accessToken,
             userData
         })
     } else {
         throw new Error('Invalid credentials!')
     }
-})
+});
+
+const getCurrent = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: false,
+        rs: user ? user : 'User not found'
+    })
+});
 
 module.exports = {
     register,
-    login
+    login,
+    getCurrent
 }
