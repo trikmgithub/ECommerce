59a85789c222b184d956982d4d88ab701ae05727
trikmgithub <trikmforwork@gmail.com>
2024-11-02 16:18:58 +0700

    Video 5

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,12 +7,49 @@ const register = asyncHandler(async (req, res) => {
             sucess: false,
             mes: 'Missing inputs'
         })
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        sucess: response ? true : false,
-        response
-    })
+
+    const user = await User.findOne({ email })
+    if (user) throw new Error('User has existed')
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            sucess: newUser ? true : false,
+            mes: newUser ? 'Register is successfully. Please go login~' : 'Something went wrong'
+        })
+    }
 })
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password)
+        return res.status(400).json({
+            sucess: false,
+            mes: 'Missing inputs'
+        })
+    // plain object
+    const response = await User.findOne({ email })
+    if (response && await response.isCorrectPassword(password)) {
+        // Tách password và role ra khỏi response
+        const { password, role, ...userData } = response.toObject()
+        // // Tạo access token
+        // const accessToken = generateAccessToken(response._id, role)
+        // // Tạo refresh token
+        // const refreshToken = generateRefreshToken(response._id)
+        // // Lưu refresh token vào database
+        // await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true })
+        // // Lưu refresh token vào cookie
+        // res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        return res.status(200).json({
+            sucess: true,
+            // accessToken,
+            userData
+        })
+    } else {
+        throw new Error('Invalid credentials!')
+    }
+})
+
 module.exports = {
-    register
+    register,
+    login
 }
